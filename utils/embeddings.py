9facb580228f4e277849549dfaf5410fdb557d90
paulohns <paulo_neves2@hotmail.com>
2025-10-28 17:56:53 -0300

    Ajustes imports

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -2,5 +2,5 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 
 def create_vectorstore(docs):
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": "cpu"})
+    embeddings = HuggingFaceEmbeddings()
     return FAISS.from_documents(docs, embeddings)
